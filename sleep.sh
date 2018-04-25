2da55fda137509aac093386233d879cf284ff76b
terest <32465023+qutaken@users.noreply.github.com>
2018-04-26 00:14:41 +0800

    Update sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -8,21 +8,23 @@ while [ $s -lt 62 ]; do
   s=$((s+1))
   sleep 0.98
   echo "run " "$y year " "$d day ,and "  "$h hour " "$m minute "  "$s second !." > timePassBy.txt
-	if [ $s -eq 60 ]; then
-		m=$((m+1))
-		s=$((0))
-		if [ $m -eq 60 ]; then
-			h=$((h+1))
-			m=$((0))
-			if [ $h -eq 24 ]; then
-				d=$((d+1))
-				h=$((0))
-				if [ $d -eq 365 ]; then
-					y=$((y+1))
-					d=$((0))
-				fi
-			fi
-		fi
+  if [ $s -eq 60 ]; then
+    m=$((m+1))
+    s=$((0))
+    if [ $m -eq 60 ]; then
+      h=$((h+1))
+      m=$((0))
+      if [ $h -eq 24 ]; then
+	d=$((d+1))
+	h=$((0))
+	if [ $d -eq 365 ]; then
+	  y=$((y+1))
+	  d=$((0))
 	fi
+      fi
+    fi
+  fi
 done
-	echo "we done " >> timePassBy.txt
+  echo "we done " >> timePassBy.txt
+  
+  
